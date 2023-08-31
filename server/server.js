5e5aee26a1f9434b673e3ffdb28b2263d3149c4a
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-31 12:32:56 -0500

    Adds API GET method

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
+app.get('/api', (req, res) => {
+    res.json({message: "Hello from my ExpressJS"});
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening onn ${PORT}`);
 })
