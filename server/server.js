c3957cd10cfa492024e2e91c5721947423c299d2
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-31 12:33:40 -0500

    Adds api/myname GET request

--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ app.get('/api', (req, res) => {
     res.json({message: "Hello from my ExpressJS"});
 });
 
+app.get('/api/myname', (req, res) => {
+    const name = {name: "Cristina Rodriguez"};
+    res.json(name);
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening onn ${PORT}`);
 })
