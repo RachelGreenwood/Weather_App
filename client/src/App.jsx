df4c9ffbd4d0a365c1bdc2f75c19b1ea963d36e5
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-31 12:36:39 -0500

    Adds way to see data from back-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,23 @@ import './App.css';
 
 function App() {
 
+  const [myName, setMyName] = useState('');
+
+  const callBackEnd = async () => {
+    const response = await fetch('http://localhost:8000/api/myname');
+    const data = await response.json();
+    console.log(data);
+    setMyName(data.name);
+  }
+
+  useEffect(() => {
+    callBackEnd();
+  }, [])
+
   return (
     <div>
       <h1>Hello Techtonica!</h1>
+      {myName}
     </div>
   )
 }
