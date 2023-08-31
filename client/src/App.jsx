5360d46f16fadff8434c0e5a04265d8434239de3
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-31 12:34:34 -0500

    Sets up front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello Techtonica!</h1>
+    </div>
+  )
+}
+
+export default App;
