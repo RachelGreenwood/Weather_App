a393320b60d88fcef7f6b3e2fe8551c8bc3bb931
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-08-31 12:54:17 -0500

    Adds access to weather API in the backend

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+import "dotenv/config";
 const cors = require('cors');
 
 const app = express();
@@ -6,13 +7,20 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/api', (req, res) => {
-    res.json({message: "Hello from my ExpressJS"});
-});
-
-app.get('/api/myname', (req, res) => {
-    const name = {name: "Cristina Rodriguez"};
-    res.json(name);
+app.get("/api", (req, res) => {
+    console.log(req.query);
+    const city = req.query.city;
+    const apiKey = process.env.API_KEY;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    console.log(url);
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            res.send({data});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 app.listen(PORT, () => {
