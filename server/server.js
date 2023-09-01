e808200343217de38c278d3c435a05005aa94404
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-01 16:17:48 -0500

    Fixes API fetch

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/api/weather", (req, res) => {
-    
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            res.send({data});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get("/api/weather", async (req, res) => {
+    const city = req.query.city;
+    const apiKey = process.env.apiKey;
+    console.log(city);
+    console.log(apiKey);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    res.json(weatherData);
+    console.log(weatherData);
 });
 
 app.listen(PORT, () => {
