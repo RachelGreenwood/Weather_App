d7ecce1a6578b6018a5fbc5d408c9bd49a064dff
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-01 13:44:59 -0500

    Fixes fetch method in back-end

--- server/server.js
+++ server/server.js
@@ -6,16 +6,15 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/api/weather", (req, res) => {
-    
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-            res.send({data});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get("/api/weather", async (req, res) => {
+    const city = req.query.city;
+    console.log(city);
+    const apiKey = "89fe2e5945f89e739118d5493ebb2f2c";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.query.city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    res.json(weatherData);
+    console.log(weatherData);
 });
 
 app.listen(PORT, () => {
