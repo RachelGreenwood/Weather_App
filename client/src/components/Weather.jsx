1ff5481eeaa05ab94a2ede8c82f9344e714265e0
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-01 13:47:45 -0500

    Adds temperature

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -5,6 +5,7 @@ const Weather = ({ data, city }) => {
                 <h2>City: {data.name}</h2>
                 <p>Description: {data.weather[0].description}</p>
                 <img src={data.weather[0].icon} />
+                <p>Temp: {data.main.temp}</p>
             </div>
         )
     }
