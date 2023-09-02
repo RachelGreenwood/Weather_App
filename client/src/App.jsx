fe6be7aff25110105c92fc95d58c6c3f9b700664
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-01 19:55:37 -0500

    Takes out console logs and adds comment for clarity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,6 @@ import Weather from "./components/Weather";
 import Form from "./components/Form";
 
 function App() {
-
-  const [myName, setMyName] = useState('');
-
-  const callBackEnd = async () => {
-    // const response = await fetch('http://localhost:8000/api/weather');
-    const response = await fetch(`http://localhost:8000/api/weather?city=${city}`);
-    const data = await response.json();
-    console.log(data.data.name);
-    setMyName(data.name);
-  }
-
   return (
     <div>
       <Form />
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Weather from "./Weather";
+
+const Form = () => {
+    const [city, setCity] = useState("");
+    const [data, setData] = useState("");
+
+    const fetchWeather = async () => {
+        const response = await fetch(
+            `http://localhost:8000/api/weather?city=${city}`
+        );
+        const weatherData = await response.json();
+        setData(weatherData);
+    };
+
+    return (
+        <div>
+            <div>
+                <input type="text" onChange={(e) => setCity(e.target.value)}></input>
+                <button onClick={fetchWeather}>See Weather</button>
+            </div>
+            <Weather city={city} data={data} />
+        </div>
+    )
+}
+
+export default Form;
