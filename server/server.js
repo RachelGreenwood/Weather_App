d18f6a0c24d2a5c82d6aa1a266920266cb98f394
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-28 13:58:16 -0500

    Adds db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+const db = require('./db/db-connection.js'); 
 
 const app = express();
 app.use(cors());
@@ -14,6 +17,44 @@ app.get("/api/weather", async (req, res) => {
     const response = await fetch(url);
     const weatherData = await response.json();
     res.json(weatherData);
+});
+
+app.get("/api/weather", async (req, res) => {
+    const city = req.query.city;
+    const apiKey = process.env.apiKey;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    res.json(weatherData);
+});
+
+app.get('/users', async (req, res) => {
+    try{
+        const { rows: users } = await db.query('SELECT * FROM users');
+        console.log("Get in the server", users);
+        res.send(users);
+
+    } catch(error){
+        console.log(error);
+        return res.status(400).json({error});
+    }
+});
+
+app.post("/api/weather", async (req, res) => {
+    try {
+        console.log("In the server", req.body);
+        const { name, city } = req.body;
+        const result = await db.query(
+            "INSERT INTO users (name, city) VALUES ($1, $2) RETURNING *",
+            [name, city]
+        );
+        let dbResponse = result.rows[0];
+        console.log(dbResponse);
+        res.json(dbResponse);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({err});
+    }
 });
 
 app.listen(PORT, () => {
