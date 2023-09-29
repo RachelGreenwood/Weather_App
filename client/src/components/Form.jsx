69e26cc106d2a33302ec1905ff3747de6769ffdb
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-29 13:46:32 -0500

    Allows user to change favorite city with PUT request

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -5,6 +5,7 @@ const Form = (props) => {
     const [city, setCity] = useState("");
     const [data, setData] = useState("");
     const [faveCity, setFaveCity] = useState("");
+    const [userId, setUserId] = useState("");
     const { name } = props;
 
     const fetchWeather = async () => {
@@ -22,7 +23,9 @@ const Form = (props) => {
         .then((response) => {
             let match = response.filter(user => user["name"] === input.value);
             console.log(match[0].city);
+            console.log(match[0].id);
             setFaveCity(match[0].city);
+            setUserId(match[0].id)
         })
     };
 
@@ -30,13 +33,29 @@ const Form = (props) => {
     // If the user's name is already in the database, get that user's id and set faveCity to their city
         // Bonus: If the user's name is already in the database, don't add the name to the DB
 
+        const handlePutRequest = () => {
+            let data = { id: userId, name, city }
+            setFaveCity(data.city);
+            fetch(`http://localhost:8000/users/${data.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            .then((response) => response.json())
+            .catch((err) => {
+                console.error("Error updating contact: ", err);
+            })
+        }
+
     return (
         <div>
             <div>
                 <input type="text" onChange={(e) => setCity(e.target.value)}></input>
                 <button onClick={fetchWeather}>See Weather</button>
             </div>
-            <Weather city={city} data={data} name={name} faveCity={faveCity} />
+            <Weather city={city} data={data} name={name} faveCity={faveCity} handlePutRequest={handlePutRequest} />
         </div>
     )
 }
--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Weather = ({ data, city, faveCity, name }) => {
+const Weather = ({ data, city, faveCity, name, handlePutRequest }) => {
     const handlePostRequest = () => {
         const postData = { name, city };
         console.log("Inside the POST, ", data);
@@ -16,6 +16,40 @@ const Weather = ({ data, city, faveCity, name }) => {
         .catch((err) => console.error("Error: ", err))
         }
 
+    // const handlePutRequest = () => {
+    //     setFaveCity(city);
+    //     fetch(`http://localhost:8000/users/${city.id}`, {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(city)
+    //     })
+    //     .then((response) => response.json())
+    //     .catch((err) => {
+    //         console.error("Error updating contact: ", err);
+    //     })
+    // }
+
+    // const handleEditContact = () => {
+    //     setEditedContact({...editedContact})
+    //     fetch(`http://localhost:8080/contacts/${editedContact.id}`, {
+    //         method: "PUT",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(editedContact)
+    //     })
+    //     .then((response) => response.json())
+    //     .then(() => {
+    //         //getRequest()
+    //         setIsEditing(false);
+    //     })
+    //     .catch((err) => {
+    //         console.error("Error updating contact: ", err);
+    //     })
+    // }
+
     if (data && city) {
         return (
             <div>
@@ -30,8 +64,11 @@ const Weather = ({ data, city, faveCity, name }) => {
                     <p>Wind Speed: {data.wind.speed}</p>
                 </div>
                 <div>
+                    {/* TODO: Make it so that if they have a favorite city, the edit button shows. Otherwise, add button shows */}
                     <p>Your favorite city is currently {faveCity}. Change to this city?</p>
-                    <button onClick={handlePostRequest}>Change Favorite City</button>
+                    <button onClick={handlePutRequest}>Change Favorite City</button>
+                    <p>If you don't have a favorite city, you can add this city</p>
+                    <button onClick={handlePostRequest}>Add City</button>
                 </div>
             </div>
         )
